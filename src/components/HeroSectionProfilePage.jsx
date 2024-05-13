095a30c07fb832701b80edb100142fcd414c7b19
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-13 18:18:12 +0200

    Btn fixed and added Epicode logo

--- src/components/HeroSectionProfilePage.jsx
+++ src/components/HeroSectionProfilePage.jsx
@@ -3,32 +3,58 @@ import { Button, Card, Col, Container, Row } from "react-bootstrap"
 const HeroSectionProfilePage = () => {
   return (
     <Card className='text-bg-dark rounded my-2'>
-      <Card.Img
-        variant='top'
-        src='https://images.unsplash.com/photo-1715464502545-090ee1dc122f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        className='img-fluid'
-      />
-      <Card.Body>
-        <Card.Title>Alessio Vulpinari</Card.Title>
-        <Card.Subtitle>Junior Full-Stack Developer</Card.Subtitle>
-        <Card.Text className='mb-2 text-muted'>EPICODE</Card.Text>
-        <Card.Text>Santarcangelo di Romagna, Emilia Romagna, Italia </Card.Text>
-        <Card.Text>Informazioni di Contatto </Card.Text>
-        <Card.Text>33 follower - 32 collegamenti </Card.Text>
+      <div className='position-relative'>
+        <Card.Img
+          variant='top'
+          src='https://images.unsplash.com/photo-1715464502545-090ee1dc122f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          className='img-fluid'
+        />
+        <div className='rounded-circle profileContainer'>
+          <img
+            className='profilePick'
+            src='https://images.unsplash.com/photo-1715114064378-b97c82f06856?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          />
+        </div>
+      </div>
 
-        <Row className='my-2'>
-          <Col xs={12} lg={"auto"}>
-            <Button variant='primary' className='me-1 rounded-pill w-100 mb-2-xs mb-0-lg'>
+      <Card.Body className='topMargin'>
+        <Row>
+          <Col xl={7} lg={6} md={12} sm={6} xs={12}>
+            <Card.Title>Alessio Vulpinari</Card.Title>
+            <Card.Subtitle>Junior Full-Stack Developer</Card.Subtitle>
+            <Card.Text className='mb-2'>EPICODE</Card.Text>
+            <Card.Text>
+              Santarcangelo di Romagna, Emilia Romagna, Italia <span> Informazioni di Contatto </span>
+            </Card.Text>
+            <Card.Text>33 follower - 32 collegamenti </Card.Text>
+          </Col>
+          <Col xl={5} lg={6} md={0} sm={6} xs={0} className='d-none d-sm-block d-md-none d-lg-block'>
+            <div className='d-flex'>
+              <img
+                src='https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1723680000&v=beta&t=zg1tmhGtXpbPBAmTL_24SZvTaU27NltAj4R2tzePhg4'
+                alt='Epicode Logo'
+                width={32}
+                height={32}
+                className='me-1'
+              />
+              <span>Epicode</span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className='my-2 row-gap-2'>
+          <Col xs={12} sm={"auto"} md={12} lg={"auto"}>
+            <Button variant='primary' className='rounded-pill w-100'>
               Disponibile per
             </Button>
           </Col>
-          <Col xs={10} lg={"auto"}>
-            <Button variant='outline-primary' className='me-1 rounded-pill'>
+          <Col xs={"auto"}>
+            <Button variant='outline-primary' className='rounded-pill'>
               Aggiungi sezione del profilo
             </Button>
           </Col>
-          <Col xs={2} lg={"auto"}>
-            <Button variant='outline-light' className='me-1 rounded-pill'>
+          <Col xs={"auto"}>
+            <Button variant='outline-light' className='rounded-pill'>
               Altro
             </Button>
           </Col>
@@ -36,9 +62,9 @@ const HeroSectionProfilePage = () => {
         <Container className='bg-secondary rounded mt-3'>
           <Row>
             <Col xs={11}>
-              <Row>
+              <Row className='py-2'>
                 <Col xs={12}>Disponibile a lavorare</Col>
-                <Col xs={12}>Ruoli di Programmatore di videogichi, ...</Col>
+                <Col xs={12}>Ruoli di Programmatore di videogiochi, ...</Col>
                 <Col xs={12}>Mostra Dettagli</Col>
               </Row>
             </Col>
