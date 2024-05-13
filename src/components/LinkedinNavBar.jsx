eabb92e671c3ffcf722432216f6e89b2e7c9dd52
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-05-13 20:11:31 +0200

    random premium text

--- src/components/LinkedinNavBar.jsx
+++ src/components/LinkedinNavBar.jsx
@@ -1,142 +1,105 @@
-import { Nav, NavDropdown, Container, Navbar, Form, Row, Col } from "react-bootstrap";
+import { Nav, Container, Navbar, Dropdown, Form, Row, Col } from "react-bootstrap";
 
 const NavBar = () => {
+  const premiumRandom = () => {
+    const text = [
+      "Raggiungi i tuoi obiettivi con Premium",
+      "Prova 1 mese di Premium per 0 EUR",
+      "Da non perdere: Premium per 0 EUR",
+      "Ottieni Premium per 0 EUR",
+      "Sblocca Premium gratis per 1 mese",
+      "Aggiorna le tue competenze con Premium",
+      "Prova Premium per 0 EUR",
+      "Una rete più smart con Premium",
+      "Prova Premium gratis",
+      "Fai decollare la tua carriera con Premium",
+      "Sblocca 1 mese di Premium",
+    ];
+    const i = Math.floor(Math.random() * text.length);
+    return text[i];
+  };
+
+  const toggleDropdown = () => {
+    const dropdownBtn = document.getElementById("dropdown-ellipsis");
+    dropdownBtn.click();
+  };
+
   return (
-    <Navbar collapseOnSelect bg="dark" variant="dark" fixed="top" className=" p-0 border-bottom">
-      <Container fluid="xl" className="justify-content-start">
-        <Navbar.Brand href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            data-supported-dps="24x24"
-            fill="currentColor"
-            className="mercado-match"
-            width="45"
-            height="45"
-            focusable="false"
-          >
-            <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-          </svg>
-        </Navbar.Brand>
-        <Form className="me-lg-auto">
-          <Row>
-            <Col xs="auto" className="d-none d-lg-block position-relative">
-              <i className="fa-solid fa-magnifying-glass nav-search-icon"></i>
+    <header className="nav-container">
+      <Navbar collapseOnSelect bg="dark" variant="dark" className="p-0" id="navbar" fixed="top">
+        <Container fluid="xl" className="justify-content-start">
+          <Nav.Link href="/" className="navbar-brand p-0 pointer d-flex justify-content-center align-items-center">
+            <i className="bi bi-linkedin navbrand-icon"></i>
+          </Nav.Link>
 
-              <Form.Control type="search " placeholder="Cerca" className="nav-search me-auto" id="nav-search-bar" />
-            </Col>
-          </Row>
-        </Form>
+          <Form className="me-lg-auto">
+            <Row>
+              <Col xs="auto" className="d-none d-lg-block position-relative">
+                <i className="bi bi-search nav-search-icon"></i>
+                <Form.Control type="search" placeholder="Cerca" className="nav-search me-auto" id="nav-searchbar" />
+              </Col>
+            </Row>
+          </Form>
 
-        <Nav>
-          <Nav.Link href="#">
-            <div className="d-flex flex-column align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                data-supported-dps="24x24"
-                fill="currentColor"
-                className="mercado-match"
-                width="24"
-                height="24"
-                focusable="false"
-              >
-                <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
-              </svg>
+          <Nav>
+            <Nav.Link href="/" className="d-flex flex-column justify-content-center align-items-center py-1">
+              <i className="bi bi-house-fill nav-icon"></i>
               <span className="d-none d-md-inline">Home</span>
-            </div>
-          </Nav.Link>
-          <Nav.Link href="#">
-            <div className="d-flex flex-column align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                data-supported-dps="24x24"
-                fill="currentColor"
-                className="mercado-match"
-                width="24"
-                height="24"
-                focusable="false"
-              >
-                <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
-              </svg>
+            </Nav.Link>
+            <Nav.Link className="d-flex flex-column justify-content-center align-items-center py-1">
+              <i className="bi bi-people-fill nav-icon"></i>
               <span className="d-none d-md-inline">Rete</span>
-            </div>
-          </Nav.Link>
-          <Nav.Link href="#">
-            <div className="d-flex flex-column align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                data-supported-dps="24x24"
-                fill="currentColor"
-                className="mercado-match"
-                width="24"
-                height="24"
-                focusable="false"
-              >
-                <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
-              </svg>
+            </Nav.Link>
+            <Nav.Link className="d-flex flex-column justify-content-center align-items-center py-1">
+              <i className="bi bi-briefcase-fill nav-icon"></i>
               <span className="d-none d-md-inline">Lavoro</span>
-            </div>
-          </Nav.Link>
-          <Nav.Link href="#">
-            <div className="d-flex flex-column align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                data-supported-dps="24x24"
-                fill="currentColor"
-                className="mercado-match"
-                width="24"
-                height="24"
-                focusable="false"
-              >
-                <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
-              </svg>
+            </Nav.Link>
+            <Nav.Link className="d-none-400 d-flex flex-column justify-content-center align-items-center py-1">
+              <i className="bi bi-chat-dots-fill nav-icon"></i>
               <span className="d-none d-md-inline">Messaggistica</span>
-            </div>
-          </Nav.Link>
-          <Nav.Link href="#">
-            <div className="d-flex flex-column align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                data-supported-dps="24x24"
-                fill="currentColor"
-                className="mercado-match"
-                width="24"
-                height="24"
-                focusable="false"
-              >
-                <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
-              </svg>
+            </Nav.Link>
+            <Nav.Link className="d-none-475 d-flex flex-column justify-content-center align-items-center py-1">
+              <i className="bi bi-bell-fill nav-icon"></i>
               <span className="d-none d-md-inline">Notifiche</span>
+            </Nav.Link>
+            <div
+              className="d-none justify-content-center align-items-center d-sm-flex"
+              style={{ borderRight: "1px solid rgba(255, 255, 255, 0.2)" }}
+            >
+              {/* <NavProfileCard /> */}
             </div>
+            <Nav.Link className="d-none d-sm-flex flex-column justify-content-center align-items-center py-1 ms-0 ms-md-3 pointer">
+              <i className="bi bi-grid-3x3-gap-fill d-flex nav-icon"></i>
+              <div className="dropdown-toggle d-none d-md-block">Per le aziende</div>
+            </Nav.Link>
+            <Nav.Link className="d-none d-md-flex flex-column justify-content-center align-items-center premium-link ms-0 ms-md-2">
+              {premiumRandom()}
+            </Nav.Link>
+          </Nav>
+
+          <Nav.Link
+            className="d-flex d-md-none flex-column justify-content-center align-items-center py-1"
+            onClick={toggleDropdown}
+          >
+            <i className="bi bi-three-dots nav-icon" style={{ WebkitTextFillColor: "rgb(255,255,255,0.55)" }}></i>
           </Nav.Link>
-          <NavDropdown title="Tu" id="basic-nav-dropdown" data-bs-theme="dark">
-            <NavDropdown.Item href="#">Profilo</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.ItemText>
-              <h5>Account</h5>
-              <NavDropdown.Item>Prova Premium per 0 EUR</NavDropdown.Item>
-              <NavDropdown.Item>Impostazioni e Privacy</NavDropdown.Item>
-              <NavDropdown.Item>Guida</NavDropdown.Item>
-              <NavDropdown.Item>Lingua</NavDropdown.Item>
-            </NavDropdown.ItemText>
-            <NavDropdown.Divider />
-            <NavDropdown.ItemText>
-              <h5>Gestisci</h5>
-              <NavDropdown.Item>Post e attività</NavDropdown.Item>
-              <NavDropdown.Item>Account per la pubblicazione di off...</NavDropdown.Item>
-            </NavDropdown.ItemText>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#">Esci</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#">Reclama 1 mese di Premium per 0 EUR</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+          <Dropdown align="end" data-bs-theme="dark">
+            <Dropdown.Toggle id="dropdown-ellipsis" className="d-none">
+              Dropdown Button
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="mt-4">
+              <div className="d-flex justify-content-center align-items-center px-1">
+                <Nav.Link className="d-flex d-sm-none flex-column justify-content-center align-items-center ms-0 ms-md-1 pointer">
+                  <i className="bi bi-grid-3x3-gap-fill d-flex nav-icon"></i>
+                  <div className="dropdown-toggle d-none d-md-block">Per le aziende</div>
+                </Nav.Link>
+                <Nav.Link className="d-flex d-md-none premium-link m-0 p-0">{premiumRandom()}</Nav.Link>
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
