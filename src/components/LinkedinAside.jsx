73ae61bf00f6186afc45347e96344fcc61fc1d99
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-05-14 11:04:23 +0200

    footer_aside

--- src/components/LinkedinAside.jsx
+++ src/components/LinkedinAside.jsx
@@ -1,179 +1,182 @@
-import { Card, Button, ListGroup, Container } from "react-bootstrap";
+import { Button, ListGroup, Container } from "react-bootstrap";
 
 function LinkedinAside() {
   return (
-    <Container id='aside' className="py-5">
-      <Card className="bg-dark text-light my-3" style={{ width: "18rem" }}>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2 justify-content-between">
-              <div>
-                <h5>Lingua del profilo</h5>
-                <p>Italiano</p>
+    <Container id="aside" className="py-5">
+      <div className="bg-dark text-light rounded my-3">
+        <Container className="p-3 pb-0">
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex justify-content-between">
+                <div>
+                  <h5>Lingua del profilo</h5>
+                  <p>Italiano</p>
+                </div>
+                <div>
+                  <Button className="editBtn">
+                    <i className="bi bi-pencil"></i>
+                  </Button>
+                </div>
               </div>
-              <div>
-                <Button className='editBtn'>
-                 <i className="bi bi-pencil"></i>
-                </Button>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex justify-content-between">
+                <div>
+                  <h5>Profilo pubblico e URL</h5>
+                  <p>www.linkedin.com/in/nome-cognome-ID</p>
+                </div>
+                <div>
+                  <Button className="editBtn">
+                    <i className="bi bi-pencil"></i>
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2 justify-content-between">
-              <div>
-                <h5>Profilo pubblico e URL</h5>
-                <p>www.linkedin.com/in/nome-cognome-ID</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Container>
+      </div>
+      <div className="bg-dark text-light rounded my-3">
+        <Container className="p-3 pb-0">
+          <h5>Persone che potresti conoscere</h5>
+          <p>Dalla tua scuola o università</p>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    className="rounded-circle"
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Cognome</a>
+                  <p>TUTTECOSE</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-person-plus-fill"></i> Collegati
+                  </Button>
+                </div>
               </div>
-              <div>
-                <Button className='editBtn'>
-                 <i className="bi bi-pencil"></i>
-                </Button>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    className="rounded-circle"
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Cognome</a>
+                  <p>TUTTECOSE</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-person-plus-fill"></i> Collegati
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card>
-      <Card className="bg-dark text-light my-3" style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Persone che potresti conoscere</Card.Title>
-          <Card.Text>Dalla tua scuola o università</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    className="rounded-circle"
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Cognome</a>
+                  <p>TUTTECOSE</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-person-plus-fill"></i> Collegati
+                  </Button>
+                </div>
               </div>
-              <div>
-                <a href="#">Nome Cognome</a>
-                <p>TUTTECOSE</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-person-plus-fill"></i> Collegati
-                </Button>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    className="rounded-circle"
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Cognome</a>
+                  <p>TUTTECOSE</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-person-plus-fill"></i> Collegati
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    className="rounded-circle"
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Cognome</a>
+                  <p>TUTTECOSE</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-person-plus-fill"></i> Collegati
+                  </Button>
+                </div>
               </div>
-              <div>
-                <a href="#">Nome Cognome</a>
-                <p>TUTTECOSE</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-person-plus-fill"></i> Collegati
-                </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Container>
+        <Button className="showBtn">Mostra tutto</Button>
+      </div>
+      <div className="bg-dark text-light rounded my-3">
+        <Container className="p-3 pb-0">
+          <h5>Potrebbe interessarti</h5>
+          <p>Pagine per te</p>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Azienda</a>
+                  <p>numero follower</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-plus-lg"></i> Segui
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark text-light border-secondary py-3">
+              <div className="d-flex gap-2">
+                <div>
+                  <img
+                    src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
+                    alt="profile picture"
+                  />
+                </div>
+                <div>
+                  <a href="#">Nome Azienda</a>
+                  <p>numero follower</p>
+                  <Button className="addBtn">
+                    <i className="bi bi-plus-lg"></i> Segui
+                  </Button>
+                </div>
               </div>
-              <div>
-                <a href="#">Nome Cognome</a>
-                <p>TUTTECOSE</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-person-plus-fill"></i> Collegati
-                </Button>
-              </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
-              </div>
-              <div>
-                <a href="#">Nome Cognome</a>
-                <p>TUTTECOSE</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-person-plus-fill"></i> Collegati
-                </Button>
-              </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
-              </div>
-              <div>
-                <a href="#">Nome Cognome</a>
-                <p>TUTTECOSE</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-person-plus-fill"></i> Collegati
-                </Button>
-              </div>
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-        <div>
-          <Button className="showBtn">Mostra tutto</Button>
-        </div>
-      </Card>
-      <Card className="bg-dark text-light my-3" style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Potrebbe interessarti</Card.Title>
-          <Card.Text>Pagine per te</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
-              </div>
-              <div>
-                <a href="#">Nome Azienda</a>
-                <p>numero follower</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-plus-lg"></i> Segui
-                </Button>
-              </div>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light">
-            <div className="d-flex gap-2">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/previews/006/732/119/non_2x/account-icon-sign-symbol-logo-design-free-vector.jpg"
-                  alt="profile picture"
-                />
-              </div>
-              <div>
-                <a href="#">Nome Azienda</a>
-                <p>numero follower</p>
-                <Button className='addBtn'>
-                  <i className="bi bi-plus-lg"></i> Segui
-                </Button>
-              </div>
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-        <div>
-          <Button className="showBtn">Mostra tutto</Button>
-        </div>
-      </Card>
+            </ListGroup.Item>
+          </ListGroup>
+        </Container>
+        <Button className="showBtn">Mostra tutto</Button>
+      </div>
     </Container>
   );
 }
--- src/components/LinkedinFooter.jsx
+++ src/components/LinkedinFooter.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Container, Row, Col, Dropdown } from 'react-bootstrap';
 
 function LinkedinFooter() {
