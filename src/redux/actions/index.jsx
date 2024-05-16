86665351f97ae09d997c0157ef67e4015a5a0791
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-05-16 12:53:22 +0200

    function jobs

--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -26,6 +26,14 @@ export const SET_FORM_BIO = "SET_FORM_BIO";
 export const SET_FORM_TITLE = "SET_FORM_TITLE";
 export const SET_FORM_AREA = "SET_FORM_AREA";
 
+//JOBS
+export const GET_ALL_JOBS = "GET_ALL_JOBS";
+export const SET_JOBS_ERROR_ON = "SET_JOBS_ERROR_ON";
+export const SET_JOBS_ERROR_OFF = "SET_JOBS_ERROR_OFF";
+export const SET_JOBS_LOADING_ON = "SET_JOBS_LOADING_ON";
+export const SET_JOBS_LOADING_OFF = "SET_JOBS_LOADING_OFF";
+export const SEARCH_JOBS = "SEARCH_JOBS";
+
 export const getMyProfile = () => {
   return async (dispatch) => {
     try {
@@ -202,6 +210,60 @@ export const changeMyProfile = (obj) => {
       console.log(error);
     } finally {
       dispatch({ type: SET_PROFILE_MODAL_OFF });
+    }
+  };
+};
+
+export const getAllJobsAction = () => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: SET_JOBS_LOADING_ON });
+      const response = await fetch("https://strive-benchmark.herokuapp.com/api/jobs", {
+        headers: {
+          authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjQxYzQ5NDE2N2U1MzAwMTVmYTY5N2YiLCJpYXQiOjE3MTU1ODYxOTYsImV4cCI6MTcxNjc5NTc5Nn0.E4rFzec_PCvcUXZGUjj_lOZjXWSmTMzgHKFcZMkg-wg",
+        },
+      });
+      if (response.ok) {
+        const jobs = await response.json();
+        dispatch({ type: GET_ALL_JOBS, payload: jobs });
+      } else {
+        if (response.status === 400) {
+          throw new Error("400: Bad Request");
+        }
+        if (response.status === 401) {
+          throw new Error("401: Unauthorized");
+        }
+        if (response.status === 402) {
+          throw new Error("402: Payment Required");
+        }
+        if (response.status === 403) {
+          throw new Error("403: Forbidden");
+        }
+        if (response.status === 404) {
+          throw new Error("404: Not Found");
+        }
+        if (response.status === 405) {
+          throw new Error("405: Method Not Allowed");
+        }
+        if (response.status === 406) {
+          throw new Error("406: Not Acceptable");
+        }
+        if (response.status === 407) {
+          throw new Error("407: Proxy Authentication Required");
+        }
+        if (response.status === 408) {
+          throw new Error("408: Request Timeout");
+        }
+        if (response.status === 500) {
+          throw new Error("500: Server Error");
+        }
+        throw new Error("Generic Fetch Error");
+      }
+    } catch (error) {
+      dispatch({ type: SET_JOBS_ERROR_ON, payload: error.message });
+    } finally {
+      dispatch({ type: SET_JOBS_LOADING_ON });
     }
   };
 };
--- src/redux/store/index.jsx
+++ src/redux/store/index.jsx
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import serchProfileReducer from "../reducers/searchMyProfileReducer";
 import postReducers from "../reducers/postReducers";
 import myProfileModalReducer from "../reducers/myProfileModalReducer";
+import jobsReducers from "../reducers/jobsReducers";
 
 const rootReducer = combineReducers({
   profile: serchProfileReducer,
   profileModal: myProfileModalReducer,
   posts: postReducers,
+  jobs: jobsReducers,
 });
 
 const store = configureStore({
