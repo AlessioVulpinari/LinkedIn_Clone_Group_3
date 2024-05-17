5b0b5d82f78406950b1e46e9396e4654769135d3
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-17 17:04:39 +0200

    fixed

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,33 +1,33 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap"
 
 function SingleComment({ comment, deleteComment }) {
   return (
     <>
       <Container>
-        <div className="d-flex gap-1 align-items-start">
+        <div className='d-flex gap-1 align-items-start'>
           <img
-            className="rounded-circle commentPic"
-            src="https://www.asiagofood.it/wp-content/uploads/2022/09/funghi-porcini-si-possono-coltivare-copertina-750x600-1.jpg"
-            alt="profile image"
+            className='rounded-circle commentPic'
+            src='https://www.asiagofood.it/wp-content/uploads/2022/09/funghi-porcini-si-possono-coltivare-copertina-750x600-1.jpg'
+            alt='profile image'
           />
-          <div>
-            <div className="comment rounded p-2 mb-1">
-              <div className="d-flex justify-content-between">
-                <a href="#784">{comment.author}</a>
+          <div className='flex-grow-1'>
+            <div className='comment rounded p-2 mb-1'>
+              <div className='d-flex justify-content-between'>
+                <a href='#784'>{comment.author}</a>
               </div>
-              <p className="text-secondary">Full Stack Web Developer</p>
-              <p className="mt-1">{comment.comment}</p>
+              <p className='text-secondary'>Full Stack Web Developer</p>
+              <p className='mt-1'>{comment.comment}</p>
             </div>
-            <Button className="commentBtn">Consiglia</Button>
-            <Button className="commentBtn">Rispondi</Button>
-            <Button className="commentBtn" onClick={() => deleteComment(comment._id)}>
+            <Button className='commentBtn'>Consiglia</Button>
+            <Button className='commentBtn'>Rispondi</Button>
+            <Button className='commentBtn' onClick={() => deleteComment(comment._id)}>
               Elimina
             </Button>
           </div>
         </div>
       </Container>
     </>
-  );
+  )
 }
 
-export default SingleComment;
+export default SingleComment
