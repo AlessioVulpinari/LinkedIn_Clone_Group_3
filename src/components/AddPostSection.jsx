e925cd519cc8a756b169be0fa7b5c00841515653
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-17 09:57:19 +0200

    minor fixed to the aside section in home

--- src/components/AddPostSection.jsx
+++ src/components/AddPostSection.jsx
@@ -1,42 +1,41 @@
-import { Container, Button } from "react-bootstrap";
-import AddPostModal from "./AddPostModal";
+import { Container, Button } from "react-bootstrap"
+import AddPostModal from "./AddPostModal"
+import { useSelector } from "react-redux"
 
 function AddPostSection() {
+  const myProfile = useSelector((state) => state.profile.content)
   return (
     <>
-      <Container id="addPostSection" className="rounded bg-dark p-3 d-flex flex-column">
-        <div className="d-flex gap-2">
+      <Container id='addPostSection' className='rounded bg-dark p-3 d-flex flex-column'>
+        <div className='d-flex gap-2'>
           <div>
-            <img
-              className="rounded-circle smallPic"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJBSetR7qpei5_qAWAdynAQxq_aYTpzLywSBTVc4MgTg&s"
-            />
+            <img className='rounded-circle smallPic' src={myProfile && myProfile.image} />
           </div>
-          <div className="w-100">
-            <Button id="addPostBtn">Avvia un post</Button>
+          <div className='w-100'>
+            <Button id='addPostBtn'>Avvia un post</Button>
           </div>
         </div>
-        <div className="d-flex justify-content-between mt-3">
+        <div className='d-flex justify-content-between mt-3'>
           <div>
-            <Button className="addContentBtn">
-              <i className="bi bi-card-image"></i> Contenuti multimediali
+            <Button className='addContentBtn'>
+              <i className='bi bi-card-image'></i> Contenuti multimediali
             </Button>
           </div>
           <div>
-            <Button className="addContentBtn">
-              <i className="bi bi-calendar2-week"></i> Evento
+            <Button className='addContentBtn'>
+              <i className='bi bi-calendar2-week'></i> Evento
             </Button>
           </div>
           <div>
-            <Button className="addContentBtn">
-              <i className="bi bi-layout-text-window-reverse"></i> Scrivi un articolo
+            <Button className='addContentBtn'>
+              <i className='bi bi-layout-text-window-reverse'></i> Scrivi un articolo
             </Button>
           </div>
         </div>
       </Container>
       <AddPostModal />
     </>
-  );
+  )
 }
 
-export default AddPostSection;
+export default AddPostSection
--- src/components/HomeAsideSx.jsx
+++ src/components/HomeAsideSx.jsx
@@ -1,78 +1,78 @@
-import { Button, ListGroup, Card } from "react-bootstrap";
+import { Button, ListGroup, Card } from "react-bootstrap"
+import { useSelector } from "react-redux"
 
 function HomeAsideSx() {
+  const myProfile = useSelector((state) => state.profile.content)
+
   return (
-    <div id="asideSx">
-      <Card className="bg-dark text-light rounded">
-        <div className="position-relative">
+    <div id='asideSx'>
+      <Card className='bg-dark text-light rounded'>
+        <div className='position-relative'>
           <Card.Img
-            variant="top"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJBSetR7qpei5_qAWAdynAQxq_aYTpzLywSBTVc4MgTg&s"
-            className="img-fluid"
+            variant='top'
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJBSetR7qpei5_qAWAdynAQxq_aYTpzLywSBTVc4MgTg&s'
+            className='img-fluid'
           />
-          <div className="rounded-circle miniProfile">
-            <img
-              className="miniPic"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJBSetR7qpei5_qAWAdynAQxq_aYTpzLywSBTVc4MgTg&s"
-            />
+          <div className='rounded-circle miniProfile'>
+            <img className='miniPic' src={myProfile && myProfile.image} />
           </div>
         </div>
-        <ListGroup variant="flush">
-          <ListGroup.Item className="bg-dark text-light border-secondary p-4 mt-5 text-center">
+        <ListGroup variant='flush'>
+          <ListGroup.Item className='bg-dark text-light border-secondary p-4 mt-5 text-center'>
             <h4>
-              <a className="toProfilePage" href="#s">
-                Nome Cognome
+              <a className='toProfilePage' href='#s'>
+                {myProfile && myProfile.name + " " + myProfile.surname}
               </a>
             </h4>
-            <p className="lightFont">tuttecosetuttecose</p>
+            <p className='lightFont'>{myProfile && myProfile.title}</p>
           </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light border-secondary px-0">
-            <Button className="cardBtn textAlign">
-              <div className="d-flex justify-content-between">
-                <p className="lightFont">Collegamenti</p>
-                <p className="lightblue">NC</p>
+          <ListGroup.Item className='bg-dark text-light border-secondary px-0'>
+            <Button className='cardBtn textAlign'>
+              <div className='d-flex justify-content-between'>
+                <p className='lightFont'>Collegamenti</p>
+                <p className='lightblue'>NC</p>
               </div>
               <p>Espandi la tua rete</p>
             </Button>
           </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light border-secondary p-0">
-            <Button className="cardBtn textAlign">
-              <p className="lightFont">Accedi a strumenti e informazioni in esclusiva</p>
-              <i className="bi bi-square-fill"></i> Prova Premium per 0 EUR
+          <ListGroup.Item className='bg-dark text-light border-secondary p-0'>
+            <Button className='cardBtn textAlign'>
+              <p className='lightFont'>Accedi a strumenti e informazioni in esclusiva</p>
+              <i className='bi bi-square-fill'></i> Prova Premium per 0 EUR
             </Button>
           </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light border-secondary p-0">
-            <Button className="cardBtn textAlign">
-              <i className="bi bi-bookmark-fill"></i> Elementi salvati
+          <ListGroup.Item className='bg-dark text-light border-secondary p-0'>
+            <Button className='cardBtn textAlign'>
+              <i className='bi bi-bookmark-fill'></i> Elementi salvati
             </Button>
           </ListGroup.Item>
         </ListGroup>
       </Card>
-      <Card className="bg-dark text-light rounded mt-2">
-        <ListGroup variant="flush">
-          <ListGroup.Item className="bg-dark text-light border-secondary p-2">
-            <a className="asideLink" href="#a">
+      <Card className='bg-dark text-light rounded mt-2'>
+        <ListGroup variant='flush'>
+          <ListGroup.Item className='bg-dark text-light border-secondary p-2'>
+            <a className='asideLink' href='#a'>
               Gruppi
             </a>
-            <div className="d-flex justify-content-between">
-              <a className="asideLink" href="#b">
+            <div className='d-flex justify-content-between'>
+              <a className='asideLink' href='#b'>
                 Eventi
               </a>
-              <Button className="rounded-circle" id="addBtn">
-                <i className="bi bi-plus-lg"></i>
+              <Button className='rounded-circle' id='addBtn'>
+                <i className='bi bi-plus-lg'></i>
               </Button>
             </div>
-            <a className="asideLink" href="#c">
+            <a className='asideLink' href='#c'>
               Hashtag seguiti
             </a>
           </ListGroup.Item>
-          <ListGroup.Item className="bg-dark text-light border-secondary p-0">
-            <Button className="cardBtn">Scopri di più</Button>
+          <ListGroup.Item className='bg-dark text-light border-secondary p-0'>
+            <Button className='cardBtn'>Scopri di più</Button>
           </ListGroup.Item>
         </ListGroup>
       </Card>
     </div>
-  );
+  )
 }
 
-export default HomeAsideSx;
+export default HomeAsideSx
